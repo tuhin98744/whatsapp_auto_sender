a139131c16555d8bbcf5818a4737b1806d5447de
tuhin98744 <62651163+tuhin98744@users.noreply.github.com>
2021-04-07 10:55:45 +0530

    Add files via upload

---
+++ auto_mssg.py
@@ -0,0 +1,10 @@
+# 1. You need to install pywhatkit. Just type (pip install pywhatkit)
+# 2. Before run the code you need to login on whatsapp web (web.whatsapp)
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg('Put the number here along with country code',
+                      'Type your message here', hour, min)
+# exaple pywhatkit.sendwhatmsg('+91123456789', 'Type your message here',13,00)
+
+# after save & run open browser and wait.
